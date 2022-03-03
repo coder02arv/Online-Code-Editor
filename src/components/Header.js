041f0d0c7e492df8333b16dd21acee3def890a03
Adarsh Singh <adarsh0004singh@gmail.com>
2022-03-03 23:12:53 +0530

    Initial

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function Header() {
+  return (
+    <div className='Header'>
+       
+       
+       <p className='companyName'>Online Code Editor</p>
+       
+    </div>
+  )
+}
